d547e932711a8860f70f69b5b1b53f5d9c0da6fb
Chad Barraford <cbarraford@gmail.com>
2014-09-29 21:36:03 -0400

    move deep symbolize to foundation

--- lib/ally/foundation.rb
+++ lib/ally/foundation.rb
@@ -32,5 +32,57 @@ module Ally
     def self.last_answer
       @chats.last.answers.last
     end
+
+    def self.deep_symbolize(h)
+      h.extend DeepSymbolizable
+      h.deep_symbolize
+    end
+  end
+end
+
+# the following was borrowed from
+# https://gist.github.com/morhekil/998709
+module DeepSymbolizable
+  def deep_symbolize(&block)
+    method = self.class.to_s.downcase.to_sym
+    syms = DeepSymbolizable::Symbolizers
+    syms.respond_to?(method) ? syms.send(method, self, &block) : self
+  end
+
+  module Symbolizers
+    # the primary method - symbolizes keys of the given hash,
+    # preprocessing them with a block if one was given, and recursively
+    # going into all nested enumerables
+    def self.hash(hash, &block)
+      hash.reduce({}) do |result, (key, value)|
+        # Recursively deep-symbolize subhashes
+        value = _recurse_(value, &block)
+
+        # Pre-process the key with a block if it was given
+        key = yield key if block_given?
+        # Symbolize the key string if it responds to to_sym
+        sym_key = key.to_sym rescue key
+
+        # write it back into the result and return the updated hash
+        result[sym_key] = value
+        result
+      end
+    end
+
+    # walking over arrays and symbolizing all nested elements
+    def self.array(ary, &block)
+      ary.map { |v| _recurse_(v, &block) }
+    end
+
+    # handling recursion - any Enumerable elements (except String)
+    # is being extended with the module, and then symbolized
+    def self._recurse_(value, &block)
+      if value.is_a?(Enumerable) && !value.is_a?(String)
+        # support for a use case without extended core Hash
+        value.extend DeepSymbolizable unless value.class.include?(DeepSymbolizable)
+        value = value.deep_symbolize(&block)
+      end
+      value
+    end
   end
 end
--- lib/ally/settings.rb
+++ lib/ally/settings.rb
@@ -16,68 +16,19 @@ module Ally
           @settings = setting.deep_merge(@settings)
         end
       end
-      @settings.extend DeepSymbolizable
-      @settings = @settings.deep_symbolize
+      @settings = Ally::Foundation.deep_symbolize(@settings)
     end
 
     def self.load!(file)
       @settings ||= {}
       config = YAML.load_file(file)
       config = config.deep_merge(@settings)
-      config.extend DeepSymbolizable
-      @settings = config.deep_symbolize
+      @settings = Ally::Foundation.deep_symbolize(config)
     end
 
     def self.method_missing(name, *args, &block)
       self.load_env_vars() unless defined?(@settings)  # load environment vars (if havent)
       settings = @settings[name.to_sym] || {}
-    end
-  end
-end
-
-# the following was borrowed from
-# https://gist.github.com/morhekil/998709
-module DeepSymbolizable
-  def deep_symbolize(&block)
-    method = self.class.to_s.downcase.to_sym
-    syms = DeepSymbolizable::Symbolizers
-    syms.respond_to?(method) ? syms.send(method, self, &block) : self
-  end
-
-  module Symbolizers
-    # the primary method - symbolizes keys of the given hash,
-    # preprocessing them with a block if one was given, and recursively
-    # going into all nested enumerables
-    def self.hash(hash, &block)
-      hash.reduce({}) do |result, (key, value)|
-        # Recursively deep-symbolize subhashes
-        value = _recurse_(value, &block)
-
-        # Pre-process the key with a block if it was given
-        key = yield key if block_given?
-        # Symbolize the key string if it responds to to_sym
-        sym_key = key.to_sym rescue key
-
-        # write it back into the result and return the updated hash
-        result[sym_key] = value
-        result
-      end
-    end
-
-    # walking over arrays and symbolizing all nested elements
-    def self.array(ary, &block)
-      ary.map { |v| _recurse_(v, &block) }
-    end
-
-    # handling recursion - any Enumerable elements (except String)
-    # is being extended with the module, and then symbolized
-    def self._recurse_(value, &block)
-      if value.is_a?(Enumerable) && !value.is_a?(String)
-        # support for a use case without extended core Hash
-        value.extend DeepSymbolizable unless value.class.include?(DeepSymbolizable)
-        value = value.deep_symbolize(&block)
-      end
-      value
     end
   end
 end
